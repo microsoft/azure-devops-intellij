8023b2b356a7bc2b91d55c72e74dba87a83bc11e
Konstantin Bulenkov <kb@jetbrains.com>
2013-11-03 23:42:32 +0100

    PlatformDataKeys -> CommonDataKeys

--- src/org/jetbrains/tfsIntegration/actions/TfsEditConfigurationAction.java
+++ src/org/jetbrains/tfsIntegration/actions/TfsEditConfigurationAction.java
@@ -2,6 +2,7 @@ package org.jetbrains.tfsIntegration.actions;
 
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
@@ -14,7 +15,7 @@ public class TfsEditConfigurationAction extends DumbAwareAction {
 
   @Override
   public void update(AnActionEvent e) {
-    Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
+    Project project = CommonDataKeys.PROJECT.getData(e.getDataContext());
     if (ActionPlaces.isPopupPlace(e.getPlace())) {
       e.getPresentation().setVisible(project != null);
     }
@@ -25,7 +26,7 @@ public class TfsEditConfigurationAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(AnActionEvent e) {
-    Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
+    Project project = CommonDataKeys.PROJECT.getData(e.getDataContext());
     ManageWorkspacesDialog d = new ManageWorkspacesDialog(project);
     d.show();
   }
