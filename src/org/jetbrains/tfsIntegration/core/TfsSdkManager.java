1a3d0adae61b0cc9f10a728282339e97ab425a82
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2015-10-16 16:04:29 +0300

    tfs: Fixed TFS SDK native libraries path detection for non-bundled TFS plugin

--- src/org/jetbrains/tfsIntegration/core/TfsSdkManager.java
+++ src/org/jetbrains/tfsIntegration/core/TfsSdkManager.java
@@ -1,6 +1,9 @@
 package org.jetbrains.tfsIntegration.core;
 
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.application.PluginPathManager;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.util.ObjectUtils;
 import com.microsoft.tfs.core.httpclient.DefaultNTCredentials;
@@ -20,10 +23,19 @@ public class TfsSdkManager {
   }
 
   private static void setupNativeLibrariesPath() {
-    File pluginHome = PluginPathManager.getPluginHome("tfsIntegration");
-    File nativeHome = new File(pluginHome, FileUtil.toSystemDependentName("lib/native"));
+    File nativeLibrariesPath = new File(getPluginDirectory(), FileUtil.toSystemDependentName("lib/native"));
 
-    System.setProperty(NativeLoader.NATIVE_LIBRARY_BASE_DIRECTORY_PROPERTY, nativeHome.getPath());
+    System.setProperty(NativeLoader.NATIVE_LIBRARY_BASE_DIRECTORY_PROPERTY, nativeLibrariesPath.getPath());
+  }
+
+  @NotNull
+  private static File getPluginDirectory() {
+    PluginId pluginId = PluginId.getId("TFS");
+    IdeaPluginDescriptor pluginDescriptor = ObjectUtils.assertNotNull(PluginManager.getPlugin(pluginId));
+
+    return pluginDescriptor.isBundled()
+           ? PluginPathManager.getPluginHome("tfsIntegration")
+           : ObjectUtils.assertNotNull(pluginDescriptor.getPath());
   }
 
   @NotNull
