22f3c126a72f33523502bebe095dce4de0b4c4c1
Yang Cao <yacao@microsoft.com>
2015-11-06 16:51:41 -0500

    Fixing the warning window on Windows machine. #459867
    Use IntelliJ's Messages class.  It supports resizing.

--- plugin.idea/src/com/microsoft/alm/plugin/idea/ui/pullrequest/CreatePullRequestController.java
+++ plugin.idea/src/com/microsoft/alm/plugin/idea/ui/pullrequest/CreatePullRequestController.java
@@ -4,15 +4,14 @@
 package com.microsoft.alm.plugin.idea.ui.pullrequest;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.ValidationInfo;
 import com.microsoft.alm.plugin.idea.resources.TfPluginBundle;
 import com.microsoft.alm.plugin.idea.ui.common.BaseDialog;
 import com.microsoft.alm.plugin.idea.ui.common.ModelValidationInfo;
 import com.microsoft.alm.plugin.idea.ui.common.ValidationListener;
-import com.microsoft.alm.plugin.idea.ui.controls.WrappingLabel;
 import git4idea.repo.GitRepository;
 
-import javax.swing.JOptionPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
@@ -110,15 +109,14 @@ public class CreatePullRequestController implements Observer, ActionListener {
 
     private boolean sanityCheckOnModel() {
         // if we have no target branches
-        if (this.createModel.getRemoteBranchDropdownModel() == null
-                || this.createModel.getRemoteBranchDropdownModel().getSize() == 0) {
+        if (this.createModel.getRemoteBranchDropdownModel().getSize() == 0) {
 
-            final WrappingLabel warningMessage = new WrappingLabel(500);
-            warningMessage.setText(TfPluginBundle.message(TfPluginBundle.KEY_CREATE_PR_NO_VALID_TARGET_WARNING_MESSAGE));
-            JOptionPane.showMessageDialog(this.createDialog.getContentPanel(),
-                    warningMessage,
-                    TfPluginBundle.message(TfPluginBundle.KEY_CREATE_PR_SANITY_CHECK_FAILED_WARNING_TITLE),
-                    JOptionPane.WARNING_MESSAGE);
+            final String warningText
+                    = TfPluginBundle.message(TfPluginBundle.KEY_CREATE_PR_NO_VALID_TARGET_WARNING_MESSAGE);
+            final String warningTitle
+                    = TfPluginBundle.message(TfPluginBundle.KEY_CREATE_PR_SANITY_CHECK_FAILED_WARNING_TITLE);
+
+            Messages.showWarningDialog(warningText, warningTitle);
 
             return false;
         }
