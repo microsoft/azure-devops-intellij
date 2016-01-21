f752a6ceb123f591c192495f756c18b998ddd644
leantk <leantk@microsoft.com>
2016-01-21 10:32:25 -0500

    Updated Windows protocol handler to open the last IDE used. This only applies to the following IDEs: IDEA, PyCharm, PhpStorm, WebStorm, RubyMine, and Android Studio If an unlisted IDE tries to register the protocol handler, nothing will happen.

--- plugin.idea/test/com/microsoft/alm/plugin/idea/setup/WindowsStartupTest.java
+++ plugin.idea/test/com/microsoft/alm/plugin/idea/setup/WindowsStartupTest.java
@@ -3,12 +3,14 @@
 
 package com.microsoft.alm.plugin.idea.setup;
 
+import com.intellij.openapi.application.ApplicationNamesInfo;
 import com.microsoft.alm.plugin.idea.IdeaAbstractTest;
 import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.WinReg;
 import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -24,14 +26,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Advapi32Util.class})
+@PrepareForTest({Advapi32Util.class, ApplicationNamesInfo.class})
 public class WindowsStartupTest extends IdeaAbstractTest {
-    private static final String TEST_INTELLIJ_ENTRY = "C:\\Program Files\\JetBRains\\IntelliJ IDEA 14.1.4\\bin\\idea.exe \"%1\"";
-    private static final String TEST_COMMAND = "C:\\Program Files\\JetBRains\\IntelliJ IDEA 14.1.4\\bin\\idea.exe vsts \"%1\"";
-    private static final String DIFFERENT_TEST_COMMAND = "C:\\Program Files\\JetBRains\\IntelliJ IDEA 15.1.4\\bin\\idea.exe vsts \"%1\"";
-    private static final String TEST_NO_EXE_STRING = "C:\\Program Files\\JetBRains\\IntelliJ IDEA 15.1.4\\bin\\exe.idea \"%1\"";
+    private static final String TEST_INTELLIJ_ENTRY = "C:\\Program Files\\JetBrains\\IntelliJ IDEA 14.1.4\\bin\\idea.exe \"%1\"";
+    private static final String TEST_COMMAND = "C:\\Program Files\\JetBrains\\IntelliJ IDEA 14.1.4\\bin\\idea.exe vsts \"%1\"";
+    private static final String DIFFERENT_TEST_COMMAND = "C:\\Program Files\\JetBrains\\IntelliJ IDEA 15.1.4\\bin\\idea.exe vsts \"%1\"";
+    private static final String TEST_NO_EXE_STRING = "C:\\Program Files\\JetBrains\\IntelliJ IDEA 15.1.4\\bin\\exe.idea \"%1\"";
     private static final String TEST_EXE_NAME = "IntellijPluginTest_" + new SimpleDateFormat("yyyyMMddhhmm").format(new Date());
     private static File TEST_EXE;
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(Advapi32Util.class);
+        PowerMockito.mockStatic(ApplicationNamesInfo.class);
+    }
 
     @After
     public void testCleanup() {
@@ -41,8 +49,25 @@ public class WindowsStartupTest extends IdeaAbstractTest {
     }
 
     @Test
+    public void testFindApplicationExeAndroidStudioExe() {
+        String studioExe = System.getProperty("os.arch").contains("64") ? "studio64.exe" : "studio32.exe";
+        Mockito.when(Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE,
+                WindowsStartup.ANDROID_STUDIO_REGISTRY_KEY, WindowsStartup.ANDROID_STUDIO_REGISTRY_VALUE)).thenReturn("C:\\Program Files\\Android\\Android Studio");
+        Assert.assertEquals("C:\\Program Files\\Android\\Android Studio\\bin\\" + studioExe + " \"%1\"", WindowsStartup.findApplicationExe("Studio"));
+    }
+
+    @Test
+    public void testFindApplicationExePhpStormExe() {
+        ApplicationNamesInfo mockApplicationNamesInfo = Mockito.mock(ApplicationNamesInfo.class);
+        Mockito.when(mockApplicationNamesInfo.getScriptName()).thenReturn("PhpStorm");
+        Mockito.when(ApplicationNamesInfo.getInstance()).thenReturn(mockApplicationNamesInfo);
+        Mockito.when(Advapi32Util.registryGetStringValue(WinReg.HKEY_CLASSES_ROOT, "Applications\\PhpStorm.exe\\shell\\open\\command", StringUtils.EMPTY)).
+                thenReturn("C:\\Program Files (x86)\\JetBrains\\PhpStorm 10.0.3\\bin\\PhpStorm.exe \"%1\"");
+        Assert.assertEquals("C:\\Program Files (x86)\\JetBrains\\PhpStorm 10.0.3\\bin\\PhpStorm.exe \"%1\"", WindowsStartup.findApplicationExe("PhpStorm"));
+    }
+
+    @Test
     public void testCheckIfKeysExistAndMatchHappyCase() {
-        PowerMockito.mockStatic(Advapi32Util.class);
         Mockito.when(Advapi32Util.registryKeyExists(WinReg.HKEY_CLASSES_ROOT, WindowsStartup.VSOI_KEY)).thenReturn(true);
         Mockito.when(Advapi32Util.registryGetStringValue(WinReg.HKEY_CLASSES_ROOT, WindowsStartup.VSOI_KEY, "")).thenReturn(TEST_COMMAND);
         Assert.assertTrue(WindowsStartup.checkIfKeysExistAndMatch(TEST_COMMAND));
@@ -50,7 +75,6 @@ public class WindowsStartupTest extends IdeaAbstractTest {
 
     @Test
     public void testCheckIfKeysExistAndMatchMismatchingExePaths() {
-        PowerMockito.mockStatic(Advapi32Util.class);
         Mockito.when(Advapi32Util.registryKeyExists(WinReg.HKEY_CLASSES_ROOT, WindowsStartup.VSOI_KEY)).thenReturn(true);
         Mockito.when(Advapi32Util.registryGetStringValue(WinReg.HKEY_CLASSES_ROOT, WindowsStartup.VSOI_KEY, "")).thenReturn(DIFFERENT_TEST_COMMAND);
         Assert.assertFalse(WindowsStartup.checkIfKeysExistAndMatch(TEST_COMMAND));
@@ -58,7 +82,6 @@ public class WindowsStartupTest extends IdeaAbstractTest {
 
     @Test
     public void testCheckIfKeysExistAndMatchNoVsoiKeyFound() {
-        PowerMockito.mockStatic(Advapi32Util.class);
         Mockito.when(Advapi32Util.registryKeyExists(WinReg.HKEY_CLASSES_ROOT, WindowsStartup.VSOI_KEY)).thenReturn(false);
         Assert.assertFalse(WindowsStartup.checkIfKeysExistAndMatch(TEST_COMMAND));
     }
