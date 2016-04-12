a58515216977f2d881641a52939512c233d8b2ab
Madhuri Gummalla <madhurig@microsoft.com>
2016-04-12 21:17:10 +0000

    PR 97214: fix bug # 565000
     - fix bug # 565000

--- plugin.idea/src/com/microsoft/alm/plugin/idea/extensions/TfGitHttpAuthDataProvider.java
+++ plugin.idea/src/com/microsoft/alm/plugin/idea/extensions/TfGitHttpAuthDataProvider.java
@@ -10,8 +10,12 @@ import com.microsoft.alm.plugin.authentication.AuthenticationInfo;
 import com.microsoft.alm.plugin.authentication.VsoAuthenticationProvider;
 import com.microsoft.alm.plugin.context.ServerContextManager;
 import git4idea.remote.GitHttpAuthDataProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TfGitHttpAuthDataProvider implements GitHttpAuthDataProvider {
+    private static final Logger logger = LoggerFactory.getLogger(TfGitHttpAuthDataProvider.class);
+
     @Override
     public AuthData getAuthData(final String url) {
         assert url != null;
@@ -23,14 +27,24 @@ public class TfGitHttpAuthDataProvider implements GitHttpAuthDataProvider {
         }
 
         //couldn't find authentication info from saved contexts
+        logger.debug("getAuthData: Couldn't find authentication info from saved contexts.");
+
         if (UrlHelper.isTeamServicesUrl(url)) {
             // We can't determine if the url is for a TFS on premise server but prompt for credentials if we know it is VSO
-            final AuthenticationInfo vsoAuthenticationInfo = AuthHelper.getAuthenticationInfoSynchronously(VsoAuthenticationProvider.getInstance(), url);
-            if (vsoAuthenticationInfo == null) {
-                //user cancelled authentication, send empty credentials to cause a auth failure
-                return new AuthData("", "");
+            // IntelliJ calls us with a http server url e.g. http://myaccount.visualstudio.com
+            // convert to https:// for team services to avoid rest call failures
+            final String authUrl = UrlHelper.getHttpsUrlFromHttpUrl(url);
+
+            if (authUrl != null) {
+                final AuthenticationInfo vsoAuthenticationInfo = AuthHelper.getAuthenticationInfoSynchronously(VsoAuthenticationProvider.getInstance(), authUrl);
+                if (vsoAuthenticationInfo == null) {
+                    //user cancelled authentication, send empty credentials to cause a auth failure
+                    return new AuthData("", "");
+                } else {
+                    return new AuthData(vsoAuthenticationInfo.getUserName(), vsoAuthenticationInfo.getPassword());
+                }
             } else {
-                return new AuthData(vsoAuthenticationInfo.getUserName(), vsoAuthenticationInfo.getPassword());
+                logger.warn("getAuthData: Unable to get https team services url for input url = " + url);
             }
         }
 
@@ -43,7 +57,19 @@ public class TfGitHttpAuthDataProvider implements GitHttpAuthDataProvider {
     public void forgetPassword(final String url) {
         // This method got called since stored credentials for the url resulted in an unauthorized error 401 or 403
         assert url != null;
-
-        ServerContextManager.getInstance().updateAuthenticationInfo(url);
+        if (UrlHelper.isTeamServicesUrl(url)) {
+            // IntelliJ calls us with a http server url e.g. http://myaccount.visualstudio.com
+            // convert to https:// for team services to avoid rest call failures
+            final String authUrl = UrlHelper.getHttpsUrlFromHttpUrl(url);
+            if (authUrl != null) {
+                ServerContextManager.getInstance().updateAuthenticationInfo(authUrl);
+            } else {
+                logger.warn("forgetPassword: Unable to get https team services url for input url = " + url);
+            }
+        } else {
+            //if onprem server is https:// we can't detect it, just use url provided by intelliJ
+            logger.warn("forgetPassword: If server is https:// user might see multiple prompts for entering password resulting in all failures.");
+            ServerContextManager.getInstance().updateAuthenticationInfo(url);
+        }
     }
 }
