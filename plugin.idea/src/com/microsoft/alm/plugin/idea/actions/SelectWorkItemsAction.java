50d2952c190f5f23b59732d70864f0e994669e7a
Leah Antkiewicz <leantk@microsoft.com>
2016-04-27 20:34:14 +0000

    PR 101531: Bug #567371 - IntelliJ: NPE in TfGitHelper.getTfGitRemote
     - Bug #567371 - IntelliJ: NPE in TfGitHelper.getTfGitRemote
    
    Related work items: #567371

--- plugin.idea/src/com/microsoft/alm/plugin/idea/actions/SelectWorkItemsAction.java
+++ plugin.idea/src/com/microsoft/alm/plugin/idea/actions/SelectWorkItemsAction.java
@@ -15,10 +15,11 @@ import com.intellij.openapi.vcs.ui.Refreshable;
 import com.microsoft.alm.plugin.idea.resources.Icons;
 import com.microsoft.alm.plugin.idea.resources.TfPluginBundle;
 import com.microsoft.alm.plugin.idea.ui.workitem.SelectWorkItemsDialog;
+import com.microsoft.alm.plugin.idea.utils.TfGitHelper;
 import org.apache.commons.lang.StringUtils;
 
 
-public class SelectWorkItemsAction extends InstrumentedAction{
+public class SelectWorkItemsAction extends InstrumentedAction {
 
     public SelectWorkItemsAction() {
         super(TfPluginBundle.message(TfPluginBundle.KEY_ACTIONS_SELECT_WORK_ITEMS_TITLE),
@@ -30,7 +31,14 @@ public class SelectWorkItemsAction extends InstrumentedAction{
     @Override
     public void doUpdate(AnActionEvent anActionEvent) {
         anActionEvent.getPresentation().setVisible(true);
-        anActionEvent.getPresentation().setEnabled(true);
+
+        // only enable button if the repo is TFS
+        final Project project = CommonDataKeys.PROJECT.getData(anActionEvent.getDataContext());
+        if ((TfGitHelper.getTfGitRepository(project) == null)) {
+            anActionEvent.getPresentation().setEnabled(false);
+        } else {
+            anActionEvent.getPresentation().setEnabled(true);
+        }
     }
 
     @Override
@@ -38,13 +46,13 @@ public class SelectWorkItemsAction extends InstrumentedAction{
         final DataContext dc = anActionEvent.getDataContext();
         final Project project = CommonDataKeys.PROJECT.getData(dc);
         final Refreshable panel = CheckinProjectPanel.PANEL_KEY.getData(dc);
-        final CommitMessageI commitMessageI = (panel instanceof CommitMessageI) ? (CommitMessageI)panel : VcsDataKeys.COMMIT_MESSAGE_CONTROL.getData(dc);
+        final CommitMessageI commitMessageI = (panel instanceof CommitMessageI) ? (CommitMessageI) panel : VcsDataKeys.COMMIT_MESSAGE_CONTROL.getData(dc);
 
         if (commitMessageI != null && project != null) {
             String commitMessage = "";
             // Attempt to append the message instead of overwriting it
             if (commitMessageI instanceof CommitChangeListDialog) {
-                commitMessage = ((CommitChangeListDialog)commitMessageI).getCommitMessage();
+                commitMessage = ((CommitChangeListDialog) commitMessageI).getCommitMessage();
             }
 
             SelectWorkItemsDialog dialog = new SelectWorkItemsDialog(project);
