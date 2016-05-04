a811cdbe2e5f0e9bea11977fdd1394a918f515ab
Yang Cao <yacao@microsoft.com>
2016-05-04 11:17:09 -0400

    Check for null instead of relying on NPE which is a bug

--- plugin/src/com/microsoft/alm/plugin/authentication/facades/VsoAuthInfoProvider.java
+++ plugin/src/com/microsoft/alm/plugin/authentication/facades/VsoAuthInfoProvider.java
@@ -8,6 +8,7 @@ import com.google.common.util.concurrent.SettableFuture;
 import com.microsoft.alm.auth.PromptBehavior;
 import com.microsoft.alm.auth.oauth.OAuth2Authenticator;
 import com.microsoft.alm.auth.pat.VstsPatAuthenticator;
+import com.microsoft.alm.oauth2.useragent.AuthorizationException;
 import com.microsoft.alm.plugin.authentication.AuthHelper;
 import com.microsoft.alm.plugin.authentication.AuthenticationInfo;
 import com.microsoft.alm.provider.JaxrsClientProvider;
@@ -56,32 +57,46 @@ public class VsoAuthInfoProvider implements AuthenticationInfoProvider {
         final SettableFuture<AuthenticationInfo> authenticationInfoFuture = SettableFuture.<AuthenticationInfo>create();
 
         try {
+            AuthenticationInfo authenticationInfo = null;
+            String errorMessage = null;
             final Client client = jaxrsClientProvider.getClient();
 
-            //TODO: this is a dependency on the aad-pat-generator jar which is going away.  When we update to consume
-            //v0.4.3 Rest client, we can get rid of this one
+            if (client != null) {
+                //TODO: this is a dependency on the aad-pat-generator jar which is going away.  When we update to consume
+                //v0.4.3 Rest client, we can get rid of this one
 
-            //Or we could reconsider the name of the token.  Now we call Profile endpoint just to get the email address
-            //which is used in token description, but do we need it?  User can only view PATs after they login, and
-            //at that time user knows which account/email they are logged in under already.  So the email provides
-            //no additional value.
-            final AccountHttpClient accountHttpClient
-                    = new AccountHttpClient(client, OAuth2Authenticator.APP_VSSPS_VISUALSTUDIO);
+                //Or we could reconsider the name of the token.  Now we call Profile endpoint just to get the email address
+                //which is used in token description, but do we need it?  User can only view PATs after they login, and
+                //at that time user knows which account/email they are logged in under already.  So the email provides
+                //no additional value.
+                final AccountHttpClient accountHttpClient
+                        = new AccountHttpClient(client, OAuth2Authenticator.APP_VSSPS_VISUALSTUDIO);
 
-            final Profile me = accountHttpClient.getMyProfile();
-            final String emailAddress = me.getCoreAttributes().getEmailAddress().getValue();
+                final Profile me = accountHttpClient.getMyProfile();
+                final String emailAddress = me.getCoreAttributes().getEmailAddress().getValue();
 
-            final String tokenDescription = AuthHelper.getTokenDescription(emailAddress);
+                final String tokenDescription = AuthHelper.getTokenDescription(emailAddress);
 
-            final Token token = vstsPatAuthenticator.getPersonalAccessToken(
-                    VsoTokenScope.or(VsoTokenScope.CodeAll, VsoTokenScope.WorkRead),
-                    tokenDescription,
-                    PromptBehavior.AUTO);
+                final Token token = vstsPatAuthenticator.getPersonalAccessToken(
+                        VsoTokenScope.or(VsoTokenScope.CodeAll, VsoTokenScope.WorkRead),
+                        tokenDescription,
+                        PromptBehavior.AUTO);
 
-            final AuthenticationInfo authenticationInfo = new AuthenticationInfo(me.getId().toString(),
-                    token.Value, serverUri, emailAddress);
+                if (token != null) {
+                    authenticationInfo = new AuthenticationInfo(me.getId().toString(),
+                            token.Value, serverUri, emailAddress);
+                } else {
+                    errorMessage = "Failed to get a Personal Access Token";
+                }
+            } else {
+                errorMessage = "Failed to get authenticated jaxrs client.";
+            }
 
-            authenticationInfoFuture.set(authenticationInfo);
+            if (authenticationInfo != null) {
+                authenticationInfoFuture.set(authenticationInfo);
+            } else {
+                authenticationInfoFuture.setException(new AuthorizationException(errorMessage));
+            }
 
         } catch (Throwable t) {
             authenticationInfoFuture.setException(t);
