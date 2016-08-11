5fb55776e8bbca99b3f5d6bc37b0324da36b78c2
leantk <leantk@microsoft.com>
2016-08-11 13:46:54 -0400

    Implementing the Add action

--- plugin.idea/src/com/microsoft/alm/plugin/idea/tfvc/core/TFSGroup.java
+++ plugin.idea/src/com/microsoft/alm/plugin/idea/tfvc/core/TFSGroup.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2008 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
 
-package org.jetbrains.tfsIntegration.core;
+package com.microsoft.alm.plugin.idea.tfvc.core;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.AbstractVcs;
@@ -22,12 +9,12 @@ import com.intellij.openapi.vcs.actions.StandardVcsGroup;
 
 public class TFSGroup extends StandardVcsGroup {
 
-  public AbstractVcs getVcs(Project project) {
-    return TFSVcs.getInstance(project);
-  }
+    public AbstractVcs getVcs(Project project) {
+        return TFSVcs.getInstance(project);
+    }
 
-  @Override
-  public String getVcsName(final Project project) {
-    return "TFS";
-  }
+    @Override
+    public String getVcsName(final Project project) {
+        return TFSVcs.TFVC_NAME;
+    }
 }
--- plugin.idea/src/com/microsoft/alm/plugin/idea/tfvc/core/TFSUpdateEnvironment.java
+++ plugin.idea/src/com/microsoft/alm/plugin/idea/tfvc/core/TFSUpdateEnvironment.java
@@ -1,209 +1,203 @@
-/*
- * Copyright 2000-2008 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
 
-package org.jetbrains.tfsIntegration.core;
+package com.microsoft.alm.plugin.idea.tfvc.core;
 
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
+//import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.update.*;
-import com.microsoft.schemas.teamfoundation._2005._06.versioncontrol.clientservices._03.*;
+//import com.intellij.openapi.vcs.VcsException;
+//import com.microsoft.schemas.teamfoundation._2005._06.versioncontrol.clientservices._03.*;
+import com.intellij.openapi.vcs.update.SequentialUpdatesContext;
+import com.intellij.openapi.vcs.update.UpdateEnvironment;
+import com.intellij.openapi.vcs.update.UpdateSession;
+import com.intellij.openapi.vcs.update.UpdatedFiles;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.tfsIntegration.core.tfs.*;
-import org.jetbrains.tfsIntegration.core.tfs.conflicts.ConflictsEnvironment;
-import org.jetbrains.tfsIntegration.core.tfs.conflicts.ResolveConflictHelper;
-import org.jetbrains.tfsIntegration.core.tfs.operations.ApplyGetOperations;
-import org.jetbrains.tfsIntegration.core.tfs.operations.ApplyProgress;
-import org.jetbrains.tfsIntegration.core.tfs.version.LatestVersionSpec;
-import org.jetbrains.tfsIntegration.core.tfs.version.VersionSpecBase;
-import org.jetbrains.tfsIntegration.exceptions.TfsException;
-import org.jetbrains.tfsIntegration.ui.UpdateSettingsForm;
+//import org.jetbrains.tfsIntegration.core.tfs.*;
+//import org.jetbrains.tfsIntegration.core.tfs.conflicts.ConflictsEnvironment;
+//import org.jetbrains.tfsIntegration.core.tfs.conflicts.ResolveConflictHelper;
+//import org.jetbrains.tfsIntegration.core.tfs.operations.ApplyGetOperations;
+//import org.jetbrains.tfsIntegration.core.tfs.operations.ApplyProgress;
+//import org.jetbrains.tfsIntegration.core.tfs.version.LatestVersionSpec;
+//import org.jetbrains.tfsIntegration.core.tfs.version.VersionSpecBase;
+//import org.jetbrains.tfsIntegration.exceptions.TfsException;
+//import org.jetbrains.tfsIntegration.ui.UpdateSettingsForm;
 
-import java.util.*;
+import java.util.Collection;
 
 public class TFSUpdateEnvironment implements UpdateEnvironment {
-  private final @NotNull TFSVcs myVcs;
+    private final
+    @NotNull
+    TFSVcs myVcs;
 
-  TFSUpdateEnvironment(final @NotNull TFSVcs vcs) {
-    myVcs = vcs;
-  }
-
-  @Override
-  public void fillGroups(final UpdatedFiles updatedFiles) {
-  }
-
-  @Override
-  @NotNull
-  public UpdateSession updateDirectories(@NotNull final FilePath[] contentRoots,
-                                         final UpdatedFiles updatedFiles,
-                                         final ProgressIndicator progressIndicator,
-                                         @NotNull final Ref<SequentialUpdatesContext> context) throws ProcessCanceledException {
-    final List<VcsException> exceptions = new ArrayList<VcsException>();
-    TFSProgressUtil.setProgressText(progressIndicator, "Request update information");
-    try {
-      final Map<WorkspaceInfo, Collection<Conflict>> workspace2Conflicts = new HashMap<WorkspaceInfo, Collection<Conflict>>();
-      List<FilePath> orphanPaths =
-        WorkstationHelper.processByWorkspaces(Arrays.asList(contentRoots), true, myVcs.getProject(),
-                                              new WorkstationHelper.VoidProcessDelegate() {
-          @Override
-          public void executeRequest(final WorkspaceInfo workspace, final List<ItemPath> paths) throws TfsException {
-            VersionSpecBase version = LatestVersionSpec.INSTANCE;
-            RecursionType recursionType = RecursionType.Full;
-            TFSProjectConfiguration configuration = TFSProjectConfiguration.getInstance(myVcs.getProject());
-            if (configuration != null) {
-              version = configuration.getUpdateWorkspaceInfo(workspace).getVersion();
-              recursionType = configuration.getState().UPDATE_RECURSIVELY ? RecursionType.Full : RecursionType.None;
-            }
-
-            // 1. query get operations for contentRoots - to let server know which version we need to report corresponding server conflicts
-            List<VersionControlServer.GetRequestParams> requests = new ArrayList<VersionControlServer.GetRequestParams>(paths.size());
-            for (ItemPath path : paths) {
-              requests.add(new VersionControlServer.GetRequestParams(path.getServerPath(), recursionType, version));
-              TFSProgressUtil.checkCanceled(progressIndicator);
-            }
-
-            List<GetOperation> operations = workspace.getServer().getVCS()
-              .get(workspace.getName(), workspace.getOwnerName(), requests, myVcs.getProject(),
-                   TFSBundle.message("preparing.for.download"));
-            // execute GetOperation-s, conflicting ones will be skipped
-            final Collection<VcsException> applyErrors = ApplyGetOperations
-              .execute(myVcs.getProject(), workspace, operations, new ApplyProgress.ProgressIndicatorWrapper(progressIndicator),
-                       updatedFiles, ApplyGetOperations.DownloadMode.ALLOW);
-            exceptions.addAll(applyErrors);
-
-            Collection<Conflict> conflicts =
-              workspace.getServer().getVCS()
-                .queryConflicts(workspace.getName(), workspace.getOwnerName(), paths, RecursionType.Full, myVcs.getProject(),
-                                TFSBundle.message("loading.conflicts"));
-
-            final Collection<Conflict> unresolvedConflicts = ResolveConflictHelper.getUnresolvedConflicts(conflicts);
-            if (!unresolvedConflicts.isEmpty()) {
-              workspace2Conflicts.put(workspace, unresolvedConflicts);
-            }
-          }
-        });
-
-      if (!workspace2Conflicts.isEmpty()) {
-        ResolveConflictHelper resolveConflictHelper = new ResolveConflictHelper(myVcs.getProject(), workspace2Conflicts, updatedFiles);
-        ConflictsEnvironment.getConflictsHandler().resolveConflicts(resolveConflictHelper);
-      }
-
-      for (FilePath orphanPath : orphanPaths) {
-        updatedFiles.getGroupById(FileGroup.UNKNOWN_ID).add(orphanPath.getPresentableUrl(), TFSVcs.getKey(), null);
-      }
-    }
-    catch (TfsException e) {
-      //noinspection ThrowableInstanceNeverThrown
-      exceptions.add(new VcsException(e));
+    TFSUpdateEnvironment(final @NotNull TFSVcs vcs) {
+        myVcs = vcs;
     }
 
-    // TODO content roots can be renamed while executing
-    TfsFileUtil.refreshAndInvalidate(myVcs.getProject(), contentRoots, false);
-
-    return new UpdateSession() {
-      @Override
-      @NotNull
-      public List<VcsException> getExceptions() {
-        return exceptions;
-      }
-
-      @Override
-      public void onRefreshFilesCompleted() {
-        myVcs.fireRevisionChanged();
-      }
-
-      @Override
-      public boolean isCanceled() {
-        return false;
-      }
-    };
-  }
-
-  @Override
-  @Nullable
-  public Configurable createConfigurable(final Collection<FilePath> files) {
-    final Map<WorkspaceInfo, UpdateSettingsForm.WorkspaceSettings> workspacesSettings =
-      new HashMap<WorkspaceInfo, UpdateSettingsForm.WorkspaceSettings>();
-    final Ref<TfsException> error = new Ref<TfsException>();
-    Runnable r = new Runnable() {
-      @Override
-      public void run() {
-        try {
-          WorkstationHelper.processByWorkspaces(files, true, myVcs.getProject(), new WorkstationHelper.VoidProcessDelegate() {
-            @Override
-            public void executeRequest(final WorkspaceInfo workspace, final List<ItemPath> paths) throws TfsException {
-              final Map<FilePath, ExtendedItem> result =
-                workspace.getExtendedItems2(paths, myVcs.getProject(), TFSBundle.message("loading.items"));
-              Collection<ExtendedItem> items = new ArrayList<ExtendedItem>(result.values());
-              for (Iterator<ExtendedItem> i = items.iterator(); i.hasNext();) {
-                final ExtendedItem extendedItem = i.next();
-                if (extendedItem == null || extendedItem.getSitem() == null) {
-                  i.remove();
-                }
-              }
-
-              if (items.isEmpty()) {
-                return;
-              }
-
-              // determine common ancestor of all the paths
-              ExtendedItem someExtendedItem = items.iterator().next();
-              UpdateSettingsForm.WorkspaceSettings workspaceSettings =
-                new UpdateSettingsForm.WorkspaceSettings(someExtendedItem.getSitem(), someExtendedItem.getType() == ItemType.Folder);
-              for (ExtendedItem extendedItem : items) {
-                final String path1 = workspaceSettings.serverPath;
-                final String path2 = extendedItem.getSitem();
-                if (VersionControlPath.isUnder(path2, path1)) {
-                  workspaceSettings = new UpdateSettingsForm.WorkspaceSettings(path2, extendedItem.getType() == ItemType.Folder);
-                }
-                else if (!VersionControlPath.isUnder(path1, path2)) {
-                  workspaceSettings = new UpdateSettingsForm.WorkspaceSettings(VersionControlPath.getCommonAncestor(path1, path2), true);
-                }
-              }
-              workspacesSettings.put(workspace, workspaceSettings);
-            }
-          });
-        }
-        catch (TfsException e) {
-          error.set(e);
-        }
-      }
-    };
-
-    ProgressManager.getInstance().runProcessWithProgressSynchronously(r, "TFS: preparing for update...", false, myVcs.getProject());
-
-    if (!error.isNull()) {
-      //noinspection ThrowableResultOfMethodCallIgnored
-      //Messages.showErrorDialog(myVcs.getProject(), error.get().getMessage(), "Update Project");
-      return null;
-    }
-    if (workspacesSettings.isEmpty()) {
-      return null;
+    @Override
+    public void fillGroups(final UpdatedFiles updatedFiles) {
     }
 
-    return new UpdateConfigurable(myVcs.getProject(), workspacesSettings);
-  }
+    @Override
+    @NotNull
+    public UpdateSession updateDirectories(@NotNull final FilePath[] contentRoots,
+                                           final UpdatedFiles updatedFiles,
+                                           final ProgressIndicator progressIndicator,
+                                           @NotNull final Ref<SequentialUpdatesContext> context) throws ProcessCanceledException {
+//    final List<VcsException> exceptions = new ArrayList<VcsException>();
+//    TFSProgressUtil.setProgressText(progressIndicator, "Request update information");
+//    try {
+//      final Map<WorkspaceInfo, Collection<Conflict>> workspace2Conflicts = new HashMap<WorkspaceInfo, Collection<Conflict>>();
+//      List<FilePath> orphanPaths =
+//        WorkstationHelper.processByWorkspaces(Arrays.asList(contentRoots), true, myVcs.getProject(),
+//                                              new WorkstationHelper.VoidProcessDelegate() {
+//          @Override
+//          public void executeRequest(final WorkspaceInfo workspace, final List<ItemPath> paths) throws TfsException {
+//            VersionSpecBase version = LatestVersionSpec.INSTANCE;
+//            RecursionType recursionType = RecursionType.Full;
+//            TFSProjectConfiguration configuration = TFSProjectConfiguration.getInstance(myVcs.getProject());
+//            if (configuration != null) {
+//              version = configuration.getUpdateWorkspaceInfo(workspace).getVersion();
+//              recursionType = configuration.getState().UPDATE_RECURSIVELY ? RecursionType.Full : RecursionType.None;
+//            }
+//
+//            // 1. query get operations for contentRoots - to let server know which version we need to report corresponding server conflicts
+//            List<VersionControlServer.GetRequestParams> requests = new ArrayList<VersionControlServer.GetRequestParams>(paths.size());
+//            for (ItemPath path : paths) {
+//              requests.add(new VersionControlServer.GetRequestParams(path.getServerPath(), recursionType, version));
+//              TFSProgressUtil.checkCanceled(progressIndicator);
+//            }
+//
+//            List<GetOperation> operations = workspace.getServer().getVCS()
+//              .get(workspace.getName(), workspace.getOwnerName(), requests, myVcs.getProject(),
+//                   TFSBundle.message("preparing.for.download"));
+//            // execute GetOperation-s, conflicting ones will be skipped
+//            final Collection<VcsException> applyErrors = ApplyGetOperations
+//              .execute(myVcs.getProject(), workspace, operations, new ApplyProgress.ProgressIndicatorWrapper(progressIndicator),
+//                       updatedFiles, ApplyGetOperations.DownloadMode.ALLOW);
+//            exceptions.addAll(applyErrors);
+//
+//            Collection<Conflict> conflicts =
+//              workspace.getServer().getVCS()
+//                .queryConflicts(workspace.getName(), workspace.getOwnerName(), paths, RecursionType.Full, myVcs.getProject(),
+//                                TFSBundle.message("loading.conflicts"));
+//
+//            final Collection<Conflict> unresolvedConflicts = ResolveConflictHelper.getUnresolvedConflicts(conflicts);
+//            if (!unresolvedConflicts.isEmpty()) {
+//              workspace2Conflicts.put(workspace, unresolvedConflicts);
+//            }
+//          }
+//        });
+//
+//      if (!workspace2Conflicts.isEmpty()) {
+//        ResolveConflictHelper resolveConflictHelper = new ResolveConflictHelper(myVcs.getProject(), workspace2Conflicts, updatedFiles);
+//        ConflictsEnvironment.getConflictsHandler().resolveConflicts(resolveConflictHelper);
+//      }
+//
+//      for (FilePath orphanPath : orphanPaths) {
+//        updatedFiles.getGroupById(FileGroup.UNKNOWN_ID).add(orphanPath.getPresentableUrl(), TFSVcs.getKey(), null);
+//      }
+//    }
+//    catch (TfsException e) {
+//      //noinspection ThrowableInstanceNeverThrown
+//      exceptions.add(new VcsException(e));
+//    }
+//
+//    // TODO content roots can be renamed while executing
+//    TfsFileUtil.refreshAndInvalidate(myVcs.getProject(), contentRoots, false);
+//
+//    return new UpdateSession() {
+//      @Override
+//      @NotNull
+//      public List<VcsException> getExceptions() {
+//        return exceptions;
+//      }
+//
+//      @Override
+//      public void onRefreshFilesCompleted() {
+//        myVcs.fireRevisionChanged();
+//      }
+//
+//      @Override
+//      public boolean isCanceled() {
+//        return false;
+//      }
+//    };
+        return null;
+    }
 
-  @Override
-  public boolean validateOptions(final Collection<FilePath> roots) {
-    return true;
-  }
+    @Override
+    @Nullable
+    public Configurable createConfigurable(final Collection<FilePath> files) {
+//    final Map<WorkspaceInfo, UpdateSettingsForm.WorkspaceSettings> workspacesSettings =
+//      new HashMap<WorkspaceInfo, UpdateSettingsForm.WorkspaceSettings>();
+//    final Ref<TfsException> error = new Ref<TfsException>();
+//    Runnable r = new Runnable() {
+//      @Override
+//      public void run() {
+//        try {
+//          WorkstationHelper.processByWorkspaces(files, true, myVcs.getProject(), new WorkstationHelper.VoidProcessDelegate() {
+//            @Override
+//            public void executeRequest(final WorkspaceInfo workspace, final List<ItemPath> paths) throws TfsException {
+//              final Map<FilePath, ExtendedItem> result =
+//                workspace.getExtendedItems2(paths, myVcs.getProject(), TFSBundle.message("loading.items"));
+//              Collection<ExtendedItem> items = new ArrayList<ExtendedItem>(result.values());
+//              for (Iterator<ExtendedItem> i = items.iterator(); i.hasNext();) {
+//                final ExtendedItem extendedItem = i.next();
+//                if (extendedItem == null || extendedItem.getSitem() == null) {
+//                  i.remove();
+//                }
+//              }
+//
+//              if (items.isEmpty()) {
+//                return;
+//              }
+//
+//              // determine common ancestor of all the paths
+//              ExtendedItem someExtendedItem = items.iterator().next();
+//              UpdateSettingsForm.WorkspaceSettings workspaceSettings =
+//                new UpdateSettingsForm.WorkspaceSettings(someExtendedItem.getSitem(), someExtendedItem.getType() == ItemType.Folder);
+//              for (ExtendedItem extendedItem : items) {
+//                final String path1 = workspaceSettings.serverPath;
+//                final String path2 = extendedItem.getSitem();
+//                if (VersionControlPath.isUnder(path2, path1)) {
+//                  workspaceSettings = new UpdateSettingsForm.WorkspaceSettings(path2, extendedItem.getType() == ItemType.Folder);
+//                }
+//                else if (!VersionControlPath.isUnder(path1, path2)) {
+//                  workspaceSettings = new UpdateSettingsForm.WorkspaceSettings(VersionControlPath.getCommonAncestor(path1, path2), true);
+//                }
+//              }
+//              workspacesSettings.put(workspace, workspaceSettings);
+//            }
+//          });
+//        }
+//        catch (TfsException e) {
+//          error.set(e);
+//        }
+//      }
+//    };
+//
+//    ProgressManager.getInstance().runProcessWithProgressSynchronously(r, "TFS: preparing for update...", false, myVcs.getProject());
+//
+//    if (!error.isNull()) {
+//      //noinspection ThrowableResultOfMethodCallIgnored
+//      //Messages.showErrorDialog(myVcs.getProject(), error.get().getMessage(), "Update Project");
+//      return null;
+//    }
+//    if (workspacesSettings.isEmpty()) {
+//      return null;
+//    }
+//
+//    return new UpdateConfigurable(myVcs.getProject(), workspacesSettings);
+        return null;
+    }
+
+    @Override
+    public boolean validateOptions(final Collection<FilePath> roots) {
+        return true;
+    }
 }
