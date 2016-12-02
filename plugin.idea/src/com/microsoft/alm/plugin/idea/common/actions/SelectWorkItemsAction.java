f9a1d657b03fdd0c2413df715ef66eeb75fcc6f6
Leah Antkiewicz <leantk@microsoft.com>
2016-12-02 21:19:41 +0000

    Merged PR 166010: AppInsights Exceptions
    -Bug #796360 - IntelliJ: NPE in AppInsights
    -Bug #796356 - IntelliJ: Illegal Arg Exception in AppInsights
    
    Related work items: #796356, #796360

--- plugin.idea/src/com/microsoft/alm/plugin/idea/common/actions/SelectWorkItemsAction.java
+++ plugin.idea/src/com/microsoft/alm/plugin/idea/common/actions/SelectWorkItemsAction.java
@@ -18,9 +18,12 @@ import com.microsoft.alm.plugin.idea.common.ui.workitem.SelectWorkItemsDialog;
 import com.microsoft.alm.plugin.idea.git.utils.TfGitHelper;
 import com.microsoft.alm.plugin.idea.tfvc.core.TFSVcs;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class SelectWorkItemsAction extends InstrumentedAction {
+    public static final Logger logger = LoggerFactory.getLogger(SelectWorkItemsAction.class);
 
     public SelectWorkItemsAction() {
         super(TfPluginBundle.message(TfPluginBundle.KEY_ACTIONS_SELECT_WORK_ITEMS_TITLE),
@@ -35,7 +38,18 @@ public class SelectWorkItemsAction extends InstrumentedAction {
 
         // only enable button if the repo is TFS
         final Project project = CommonDataKeys.PROJECT.getData(anActionEvent.getDataContext());
-        if (TfGitHelper.getTfGitRepository(project) == null && TFSVcs.getInstance(project) == null) {
+
+        boolean disableButton;
+        try {
+            // project might be null or in an unexpected state if it not a TFS repo which causes exceptions to be thrown
+            // this is a weird situation since the Commit dialog can be brought up in many situations
+            disableButton = (project == null || (TfGitHelper.getTfGitRepository(project) == null && TFSVcs.getInstance(project) == null));
+        } catch (Exception e) {
+            logger.warn("Exception finding if project is TFS for work item association", e);
+            disableButton = true;
+        }
+
+        if (disableButton) {
             anActionEvent.getPresentation().setEnabled(false);
             // change hover text to explain why button is disabled
             anActionEvent.getPresentation().setText(TfPluginBundle.message(TfPluginBundle.KEY_ERRORS_NOT_TFS_REPO,
