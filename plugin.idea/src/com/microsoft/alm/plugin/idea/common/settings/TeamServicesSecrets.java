63c9a642081750e915804e1a6e8c78abdd77bfc8
Leah Antkiewicz <leantk@microsoft.com>
2017-08-25 20:25:22 +0000

    Merged PR 251702: User Story #1062164 - As a user, I would like multiple password to be saved
    User Story #1062164 - As a user, I would like multiple password to be saved multiple repos
    
    Related work items: #1062164

--- plugin.idea/src/com/microsoft/alm/plugin/idea/common/settings/TeamServicesSecrets.java
+++ plugin.idea/src/com/microsoft/alm/plugin/idea/common/settings/TeamServicesSecrets.java
@@ -3,6 +3,8 @@
 
 package com.microsoft.alm.plugin.idea.common.settings;
 
+import com.intellij.credentialStore.CredentialAttributes;
+import com.intellij.credentialStore.Credentials;
 import com.intellij.ide.passwordSafe.PasswordSafe;
 import com.intellij.ide.passwordSafe.PasswordSafeException;
 import com.microsoft.alm.plugin.authentication.AuthenticationInfo;
@@ -48,17 +50,47 @@ public class TeamServicesSecrets {
         writePassword(key, stringValue);
     }
 
-    public static void forgetPassword(final String key) {
+    /**
+     * Forget password by overwriting existing password with null
+     *
+     * @param key
+     */
+    private static void forgetPassword(final String key) {
+        writePassword(key, null);
+    }
+
+    /**
+     * Writing password to the IDEA credential store
+     * <p>
+     * Attempt to write the password using the new IDEA method
+     * If that doesn't exist it will fall back to the old way which doesn't store multiple passwords and is deprecated
+     *
+     * @param key
+     * @param value
+     */
+    private static void writePassword(final String key, final String value) {
+        // try writing password using the new IDEA implementation
         try {
-            PasswordSafe.getInstance().removePassword(null, TeamServicesSecrets.class, key);
-        } catch (PasswordSafeException e) {
-            logger.warn("Failed to clear password store", e);
-        } catch (Throwable t) {
-            logger.warn("Failed to clear password store", t);
+            // use key as the serviceName (not username) for CredentialAttributes because that is the the unique identifier used to save it in the store
+            final CredentialAttributes attributes = new CredentialAttributes(key);
+            final Credentials credentials = new Credentials(key, value);
+            PasswordSafe.getInstance().set(attributes, credentials);
+        } catch (final NoClassDefFoundError error) {
+            logger.warn("Failed to write password using new implementation so attempting old way", error);
+            writePasswordOldWay(key, value);
+        } catch (final NoSuchMethodError error) {
+            logger.warn("Failed to write password using new implementation so attempting old way", error);
+            writePasswordOldWay(key, value);
         }
     }
 
-    public static void writePassword(final String key, final String value) {
+    /**
+     * Old way to write passwords which is deprecated and should only be used in older version of IDEA
+     *
+     * @param key
+     * @param value
+     */
+    private static void writePasswordOldWay(final String key, final String value) {
         try {
             PasswordSafe.getInstance().storePassword(null, TeamServicesSecrets.class, key, value);
         } catch (PasswordSafeException e) {
@@ -68,15 +100,45 @@ public class TeamServicesSecrets {
         }
     }
 
-    public static String readPassword(final String key) {
+    /**
+     * Reading password from the IDEA credential store
+     * <p>
+     * Attempt to read the password using the new IDEA method
+     * If that doesn't exist it will fall back to the old way which is deprecated
+     *
+     * @param key
+     * @return unencrypted password or and empty string if no password is found
+     */
+    private static String readPassword(final String key) {
         String password = StringUtils.EMPTY;
+        // try reading password using the new IDEA implementation
         try {
-            password = PasswordSafe.getInstance().getPassword(null, TeamServicesSecrets.class, key);
-        } catch (PasswordSafeException e) {
+            final CredentialAttributes attributes = new CredentialAttributes(key);
+            final Credentials credentials = PasswordSafe.getInstance().get(attributes);
+            password = credentials != null ? credentials.getPasswordAsString() : password;
+        } catch (final NoClassDefFoundError error) {
+            logger.warn("Failed to get password using new implementation so attempting old way", error);
+            password = readPasswordOldWay(key);
+        } catch (final NoSuchMethodError error) {
+            logger.warn("Failed to get password using new implementation so attempting old way", error);
+            password = readPasswordOldWay(key);
+        }
+        return password;
+    }
+
+    /**
+     * Old way to read passwords which is deprecated and should only be used in older version of IDEA
+     *
+     * @param key
+     */
+    private static String readPasswordOldWay(final String key) {
+        try {
+            return PasswordSafe.getInstance().getPassword(null, TeamServicesSecrets.class, key);
+        } catch (final PasswordSafeException e) {
             logger.warn("Failed to read password", e);
         } catch (Throwable t) {
             logger.warn("Failed to read password", t);
         }
-        return password;
+        return StringUtils.EMPTY;
     }
 }
