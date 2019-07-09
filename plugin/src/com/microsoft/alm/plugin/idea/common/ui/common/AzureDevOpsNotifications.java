faab467fdc4b3d5484e01dae08bbd5e9b478ca8e
Ivan Migalev <ivan.migalev@jetbrains.com>
2019-07-09 12:22:24 +0700

    Add headers to the new files (#205)

--- plugin/src/com/microsoft/alm/plugin/idea/common/ui/common/AzureDevOpsNotifications.java
+++ plugin/src/com/microsoft/alm/plugin/idea/common/ui/common/AzureDevOpsNotifications.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
 package com.microsoft.alm.plugin.idea.common.ui.common;
 
 import com.intellij.notification.Notification;
