a04b369db7b7581c17e1f807afaede24cb66c641
Ivan Migalev <ivan.migalev@jetbrains.com>
2019-09-23 15:03:46 +0700

    Add Rd protocol for the reactive client

--- client/backend/src/main/kotlin/com/microsoft/tfs/Main.kt
+++ client/backend/src/main/kotlin/com/microsoft/tfs/Main.kt
@@ -1,20 +1,34 @@
 package com.microsoft.tfs
 
-import com.microsoft.tfs.core.httpclient.UsernamePasswordCredentials
-import java.nio.file.Paths
+import com.jetbrains.rd.framework.*
+import com.jetbrains.rd.util.lifetime.LifetimeDefinition
+import com.jetbrains.rd.util.threading.SingleThreadScheduler
+import com.microsoft.tfs.model.host.TfsRoot
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val login = args[0]
-    val password = args[1]
-    val path = Paths.get(System.getProperty("user.dir"))
-
-    val client = TfsClient(path, UsernamePasswordCredentials(login, password))
-    val pending = client.status(path)
-    pending.forEach {
-        println(it.pendingChanges.size)
-        println(it.candidatePendingChanges.size)
-        for (candidatePendingChange in it.candidatePendingChanges) {
-            println(candidatePendingChange)
-        }
+    val port = args[0].toIntOrNull()
+    if (port == null) {
+        printUsage()
+        exitProcess(1)
     }
+
+    runRdClient(port)
+}
+
+private fun printUsage() {
+    println("Reactive TFS Client ver. 1.0")
+    println("Command-line options:")
+    println("  <portNumber>: port to connect to")
+}
+
+private fun runRdClient(portNumber: Int) {
+    val appLifetimeDefinition = LifetimeDefinition()
+    val appLifetime = appLifetimeDefinition.lifetime
+    val scheduler = SingleThreadScheduler(appLifetime, "com.microsoft.tfs.MainScheduler")
+    val socket = SocketWire.Client(appLifetime, scheduler, portNumber, "com.microsoft.tfs.MainSocketClient")
+    val protocol = Protocol(Serializers(), Identities(IdKind.Client), scheduler, socket, appLifetime)
+    val model = TfsRoot.create(appLifetime, protocol)
+
+    model.shutdown.advise(appLifetime) { appLifetimeDefinition.terminate() }
 }
