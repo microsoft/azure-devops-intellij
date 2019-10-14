30bb04b2f1430fa86d52953de2621c9cbd950769
Ivan Migalev <ivan.migalev@jetbrains.com>
2019-10-14 14:28:05 +0700

    Add a setting to enable reactive client

--- plugin/src/com/microsoft/alm/plugin/services/PropertyService.java
+++ plugin/src/com/microsoft/alm/plugin/services/PropertyService.java
@@ -3,12 +3,25 @@
 
 package com.microsoft.alm.plugin.services;
 
+import com.microsoft.alm.plugin.idea.tfvc.core.ClassicTfvcClient;
+import com.microsoft.alm.plugin.idea.tfvc.core.ReactiveTfvcClient;
+import org.jetbrains.annotations.NotNull;
+
 public interface PropertyService {
     String PROP_LAST_CONTEXT_KEY = "lastContextKey";
     String PROP_REACTIVE_CLIENT_PATH = "reactiveClientPath";
+    String PROP_TFVC_CLIENT_TYPE = "tfvcClientType";
     String PROP_REPO_ROOT = "repoRoot";
     String PROP_TF_HOME = "tfHome";
     String PROP_AUTH_TYPE = "authType";
+
+    String CLIENT_TYPE_CLASSIC = ClassicTfvcClient.class.getName();
+    String CLIENT_TYPE_REACTIVE = ReactiveTfvcClient.class.getName();
+
+    @NotNull
+    static PropertyService getInstance() {
+        return PluginServiceProvider.getInstance().getPropertyService();
+    }
 
     String getProperty(String propertyName);
 
