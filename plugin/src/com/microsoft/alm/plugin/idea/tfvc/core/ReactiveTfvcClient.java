9dffd61e788abde21bf1711c65df356434c3777b
Ivan Migalev <ivan.migalev@jetbrains.com>
2019-10-14 17:32:36 +0700

    Add copyright headers to all the new files

--- plugin/src/com/microsoft/alm/plugin/idea/tfvc/core/ReactiveTfvcClient.java
+++ plugin/src/com/microsoft/alm/plugin/idea/tfvc/core/ReactiveTfvcClient.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
 package com.microsoft.alm.plugin.idea.tfvc.core;
 
 import com.intellij.openapi.diagnostic.Logger;
