bd4ca39f9137afd0d7da6dbbf5c6af5f6837541f
Ivan Migalev <ivan.migalev@jetbrains.com>
2020-07-10 18:06:39 +0700

    Share lifetime between host, protocol and connection
    This will allow us to get rid of errors when closing a project.

--- plugin/src/com/microsoft/alm/plugin/external/reactive/Lifetimes.java
+++ plugin/src/com/microsoft/alm/plugin/external/reactive/Lifetimes.java
@@ -5,12 +5,11 @@ package com.microsoft.alm.plugin.external.reactive;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.util.Disposer;
-import com.jetbrains.rd.util.lifetime.Lifetime;
 import com.jetbrains.rd.util.lifetime.LifetimeDefinition;
 import com.jetbrains.rd.util.lifetime.LifetimeStatus;
 
 public class Lifetimes {
-    public static Lifetime defineNestedLifetime(Disposable disposable) {
+    public static LifetimeDefinition defineNestedLifetime(Disposable disposable) {
         LifetimeDefinition lifetimeDefinition = new LifetimeDefinition();
         Disposer.register(disposable, () -> {
             if (lifetimeDefinition.getStatus() == LifetimeStatus.Alive)
